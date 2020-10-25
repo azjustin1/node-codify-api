72b58c68048a147bd8498986b1d5574bb5f60891
azjustin1 <48974108+azjustin1@users.noreply.github.com>
2020-10-25 15:42:55 +0700

    jwt authenticate with account active status

--- models/User.js
+++ models/User.js
@@ -1,60 +1,76 @@
-import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
-import Joi from 'joi'
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import Joi, { boolean } from "joi";
+import JWT from "jsonwebtoken";
+import dotenv from "dotenv";
 
-const Schema = mongoose.Schema
+dotenv.config();
+
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    imgUrl: {
-        type: String,
-    }
-})
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-userSchema.pre('save', function (next) {
-    // Check if the user already have password
-    if (!this.isModified('password')) {
-        return next()
-    }
-    // Hash the password before save user
-    bcrypt.genSalt(10, async (err, salt) => {
-        if (err) return next(err)
-        bcrypt.hash(this.password, salt, (err, hash) => {
-            if (err) return next(err)
-            this.password = hash
-            return next()
-        })
-    })
-
-})
-
-userSchema.methods.Validate = function (obj) {
-    const schema = Joi.object().keys({
-        email: Joi.string().email().min(6).required(),
-        password: Joi.string().min(6).required(),
-    })
-    const validation = schema.validate(obj)
-    return validation
-}
-
-userSchema.methods.comparePassword = function (candidatePassword, next) {
-    const password = this.password
-    bcrypt.compare(candidatePassword, password, function (err, isMatch) {
-        if (err) {
-            console.log('Wrong password')
-            next(null, false)
-        }
-        return next(null, isMatch);
+userSchema.pre("save", function (next) {
+  // Check if the user already have password
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // Hash the password before save user
+  bcrypt.genSalt(10, async (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      return next();
     });
+  });
+});
+
+// Validate user input
+userSchema.methods.Validate = function (obj) {
+  const schema = Joi.object().keys({
+    email: Joi.string().email().min(6).required(),
+    password: Joi.string().min(6).required(),
+  });
+  const validation = schema.validate(obj);
+  return validation;
 };
 
+userSchema.methods.comparePassword = function (candidatePassword, next) {
+  const password = this.password;
+  bcrypt.compare(candidatePassword, password, function (err, isMatch) {
+    if (err) {
+      console.log("Wrong password");
+      next(null, false);
+    }
+    return next(null, isMatch);
+  });
+};
 
-export default mongoose.model('User', userSchema)
+userSchema.methods.generateAccessToken = function () {
+  console.log(this.email, this.active);
+  const payload = { _id: this._id, email: this.email, active: this.active };
+  const token = JWT.sign({ payload: payload }, process.env.SECRET_TOKEN, {
+    expiresIn: 3600,
+  });
+  return token;
+};
+
+export default mongoose.model("User", userSchema);
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import fs from "fs";
 import { execFile } from "child_process";
@@ -22,7 +21,7 @@ router.post("/signup", (req, res, next) => {
     "signup",
     {
       session: false,
-      badRequestMessage: "Please enter all the infomation",
+      badRequestMessage: "Please enter all the information",
     },
     async (err, user, info) => {
       if (err) return res.status(422).send(info);
@@ -43,123 +42,16 @@ router.post("/signin", (req, res, next) => {
         return res.status(422).send(info);
       }
       req.login(user, { session: false }, (err) => {
-        if (err) res.status(401).send(info);
+        if (err) res.status(403);
+        // Receive access token from passport middleware and send to client
+        return res.status(200).send(info);
       });
-      const body = { _id: user._id, email: user.email };
-      const token = JWT.sign({ user: body }, process.env.SECRET_TOKEN, {
-        expiresIn: 3600,
-      });
-
-      return res.status(200).send({ token });
     }
   )(req, res, next);
 });
 
 router.post("/run", (req, res, next) => {
-  // Create .c file to make it runnable with other child process
-  const fileName = `code.c`;
-  const writeStream = fs.createWriteStream(fileName);
-  // Get code from the client and write to a file
-  writeStream.write(req.body.code);
-  writeStream.end();
-  // Build created code file and make it runnable
-  try {
-    const child = execFile(
-      "gcc",
-      [fileName, "-o", "code"],
-      (error, stdout, stderr) => {
-        if (error) {
-          res.send(stderr);
-        }
-        // res.send(stdout);
-        console.log(stdout);
-        // Run code.exe and send the result to the client
-        try {
-          const child = execFile("./code", (error, stdout, stderr) => {
-            if (error) {
-              throw error;
-            }
-            const result = stdout;
-            // Delete 2 code files in the server after send
-            try {
-              // fs.unlinkSync("./code.exe");
-              // fs.unlinkSync("./code.c");
-              //file removed
-            } catch (err) {
-              console.error(err);
-            }
-            res.send(result);
-          });
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    );
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-export default router;
-=======
-import express from "express";
-import fs from "fs";
-import { execFile } from "child_process";
-import passport from "passport";
-import JWT from "jsonwebtoken";
-import dotenv from "dotenv";
-// import { io } from '../server'
-//This is custom passport
-import authRouter from "./authRoutes";
-
-dotenv.config();
-
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.status(200).send({ message: "express is up" });
-});
-
-router.post("/signup", (req, res, next) => {
-  passport.authenticate(
-    "signup",
-    {
-      session: false,
-      badRequestMessage: "Please enter all the infomation",
-    },
-    async (err, user, info) => {
-      if (err) return res.status(422).send(info);
-      return res.status(200).send(info);
-    }
-  )(req, res, next);
-});
-
-router.post("/signin", (req, res, next) => {
-  passport.authenticate(
-    "signin",
-    {
-      session: false,
-      badRequestMessage: "Please enter email and password to sign in",
-    },
-    async (err, user, info) => {
-      if (err || !user) {
-        return res.status(422).send(info);
-      }
-      req.login(user, { session: false }, (err) => {
-        if (err) res.status(401).send(info);
-      });
-      const body = { _id: user._id, email: user.email };
-      const token = JWT.sign({ user: body }, process.env.SECRET_TOKEN, {
-        expiresIn: 3600,
-      });
-
-      return res.status(200).send({ token });
-    }
-  )(req, res, next);
-});
-
-router.post("/run", (req, res, next) => {
-  const fileName = "code.cpp";
+  const fileName = "code.c";
   const writeStream = fs.createWriteStream(fileName);
   // Get code from the client and write to a file
   writeStream.write(req.body.code);
@@ -183,7 +75,7 @@ router.post("/run", (req, res, next) => {
         // Delete 2 code files in the server after send
         try {
           fs.unlinkSync("./code.exe");
-          fs.unlinkSync("./code.cpp");
+          fs.unlinkSync("./code.c");
           //file removed
         } catch (err) {
           console.error(err);
@@ -195,4 +87,3 @@ router.post("/run", (req, res, next) => {
 });
 
 export default router;
->>>>>>> a4c7b4bc36cce17eae0d165018948c5dca2efcb8
